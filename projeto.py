1fe99a0dcbc0ac8d40bfb46b2f901b7660403b69
Meninoantonio <marcosantonioer014@gmail.com>
2025-05-26 21:48:53 -0300

    26/05

--- projeto.py
+++ projeto.py
@@ -1,4 +1,37 @@
-print("Hello world")
+import unicodedata
+
+nome = input("Digite seu nome: ")
+
+
+nome_normalizado = unicodedata.normalize('NFD', nome)
+nome_sem_acentos = ''.join(c for c in nome_normalizado if unicodedata.category(c) != 'Mn')
+nome_minusculo = nome_sem_acentos.lower()
+
+nome = nome_minusculo
+nome = nome_normalizado
+nome = nome_sem_acentos.lower()
+
+vogal = 0
+consonante = 0
+
+print(nome)
+for letra in nome:
+
+        if (letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u"or letra == " "):
+            vogal += 1
+            if (letra == " "):
+                vogal -= 1
+        else:
+            consonante += 1
+
+print("O seu nome tem tantas vogais %d, e tantas consonante %d"%(vogal,consonante))
+
+familia = ["Joao P","Maria","Joao M","Marcos"]
+for presente in familia:
+    print
+for num in range(1,5):
+    print
+print(num,presente)
 
 
 
