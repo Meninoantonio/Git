9967a820ffbdd5cf7f94e8fb3f1e2a7f5325483b
Meninoantonio <marcosantonioer014@gmail.com>
2025-08-05 21:42:20 -0300

    05/08

--- projeto.py
+++ projeto.py
@@ -32,8 +32,51 @@ for presente in familia:
 for num in range(1,5):
     print
 print(num,presente)
+_________________________________________________________________________________________________________________________________________________________________________________________
+print("Calculador de IMC")
+
+nome = input("Qual o seu nome? ").capitalize()
+idade = int(input("Qual a sua idade? "))
+altura = int(input("Em centimetros qual a sua idade? (170) "))
+genero = (input("Qual o seu genero de nascimento? (M ou F) ")).upper()
+peso = float(input("Qual o seu peso? (Ex: 50) "))
 
 
+print(f"Olá {nome}, seu peso é {peso} e altura {altura} ")
+ 
+altura = altura/100
+
+if (genero == "M") :
+    imc = round( peso/(altura*altura), 2)
+    if imc < 18.5:
+        print("Sua classificação é: Baixo Peso ")
+    elif 18.5 <= imc <= 24.9:
+       print("Sua classificação é: normal ")
+    elif 25.0 <= imc <= 29.9:
+       print("Sua classificação é: excesso de peso ")
+    elif 30.0 <= imc <= 34.9 :
+       print("Sua classificação é: Obesidade 1 ")
+    elif 35 <= imc <= 39.9:
+       print("Sua classificação é: Obesidade 2 ")
+    elif imc >= 40.0 :
+        print("Sua classificacao é Obesidade 3 ")
+    print(f"Seu IMC é {imc} ")
+    
+else: 
+    imc = round( peso/(altura*altura), 2)
+    if imc < 18.5:
+        print("Sua classificação é: Baixo Peso ")
+    elif 18.5 <= imc <= 24.9:
+       print("Sua classificação é: normal ")
+    elif 25.0 <= imc <= 29.9:
+       print("Sua classificação é: excesso de peso ")
+    elif 30.0 <= imc <= 34.9 :
+       print("Sua classificação é: Obesidade 1 ")
+    elif 35 <= imc <= 39.9:
+       print("Sua classificação é: Obesidade 2 ")
+    elif imc >= 40.0 :
+        print("Sua classificacao é Obesidade 3 ")
+    print(f"Seu IMC é {imc} ")
 
 
 
